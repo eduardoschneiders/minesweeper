ecad201d72a3c0bf58ab18573c51348b880e6b4d
Eduardo Schneiders <eduardo.schneiders@lemoney.com>
2021-07-10 23:53:44 -0300

    Feat(Game board): Draw grid system

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,121 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+class Cell extends Component {
+  render () {
+    let content = null
+
+    if (this.props.status === 'covered') {
+      content = 'C'
+    } else if (this.props.status === 'flagged' ) {
+      content = 'F'
+    } else {
+      content = this.props.value
+    }
+
+    return (
+      <div
+        className="cell"
+        onContextMenu={ (e) => e.preventDefault() }
+        onMouseDown={
+          (e) => {
+            this.props.onClick(e.buttons)
+          }
+        }
+      >
+        {content}
+      </div>
+    )
+  }
+}
+
+class Board extends Component {
+  constructor(props) {
+    super(props)
+
+    let rowsCount = 10
+    let collumnsCount = 15
+    let grid = []
+    for (let i = 0; i < rowsCount; i++) {
+      let row = Array(collumnsCount).fill({ value: 0, status: 'covered'})
+      grid.push(row)
+    }
+
+    this.state = {
+      grid: grid
+    }
+  }
+
+  handleClick(buttons, i, j) {
+    let grid = this.state.grid.slice();
+    let status = null
+
+    if (buttons === 1) {
+      status = 'uncovered'
+    } else if (buttons === 2) {
+      if (grid[i][j]['status'] === 'flagged') {
+        status = 'covered'
+      } else {
+        status = 'flagged'
+      }
+    }
+
+    grid[i][j] = { value: 2, status: status }
+
+    this.setState({ grid: grid })
+  }
+
+  render () {
+    return (
+      <div>
+        {
+          this.state.grid.map((row, i) => {
+              return(
+                <ul className="row" key={i}>
+                  {
+                    row.map((cel, j) => {
+                        return(
+                          <Cell
+                            key={j}
+                            value={cel['value']}
+                            status={cel['status']}
+                            onClick = {
+                              (buttons) => {
+                                this.handleClick(buttons, i, j)
+                              }
+                            }
+                          />
+                        )
+                      }
+                    )
+                  }
+                </ul>
+              )
+            }
+          )
+        }
+      </div>
+    )
+  }
+}
+
+class Game extends Component {
+  render () {
+    return(
+      <div className="game">
+        <div className="game-board">
+            <Board />
+        </div>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
 
